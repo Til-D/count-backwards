9c7c1b9e2230308186cecec6d077974d1374a717
tilman <tilman@tilmanification.com>
2018-04-30 18:34:09 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Count Backwards Task
+========
+
+An implementation of a count backwarks tasks to induce stress under lab conditions.
+
+## General information
+The test is intended to challenge participants to count backwards in various decrements from a random 3-digit number. It is inspired by the 'Serial Sevens' tasks, which is used in clinical tests to assess mental functions. While the participant follows the instructions on the screen and calls out the sequence of numbers, the experimenter puts additional pressure on the subject by requesting to hurry up. If the participant calls out a wrong number, the experiementer intervenes by pressing a key and the task is reset.
+
+## Installation
+No installation required. Just [download](https://github.com/Til-D/count-backwards/archive/master.zip) the ZIP file, unzip it and open `index.html` with a browser of your choice.
+
+## Author
+- [Tilman Dingler](https://github.com/Til-D/)
+
+## Used libraries and utilities
+- [jQuery](http://jquery.com/) ([MIT license](https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt))
+- [Bootstrap](https://getbootstrap.com/) ([MIT license](http://www.opensource.org/licenses/mit-license) 
+
+## License
+This count backwards implementation is published under the [MIT license](http://www.opensource.org/licenses/mit-license) and [GPL v3](http://opensource.org/licenses/GPL-3.0).
---
+++ js/custom.js
@@ -0,0 +1,119 @@
+$( document ).ready(function() {
+
+	const DECREMENTS = [6, 7, 8, 9];
+	const IMAGEFOLDER = 'images/'
+	const FAILIMAGES = ['1.jpg', '2.jpg', '3.png', '4.png', '5.png', '6.png'];
+	const SECOND = 1000; //ms
+	const TIMEOUT = 2 * SECOND;
+	const COUNTSTART = 3;
+	const READYTEXT = "Get ready .";
+	const SHOWFAILIMAGES = false;
+	const STRESSINTERVAL_MIN = 5 * SECOND;
+	const STRESSINTERVAL_MAX = 25 * SECOND;
+	const STRESS_WINDOW_DURATION = 1 * SECOND;
+
+	var startNumber,
+		decrement,
+		count,
+		inProgress;
+	
+
+	$("#btnGo").click(function() {
+		init();
+	});
+
+	$("#btnRestart").click(function() {
+		init();
+	});
+
+	$(document).keypress(function(e) {
+		if(inProgress) {
+	  		console.log('key pressed: ' + e.which);
+	  		fail();
+	  	}
+	});
+
+	function init() {
+
+		$("#instructions").hide();
+
+		startNumber = Math.floor((Math.random() * 600) + 300);
+		decrement = DECREMENTS[Math.floor(Math.random()*DECREMENTS.length)];		
+
+		$("#startNumber").html(startNumber);
+		$("#decrement").html(decrement);
+
+		console.log("startNumber: " + startNumber);
+		console.log("decrement: " + decrement);
+
+		
+		count = COUNTSTART;
+		$("#readyText").html(READYTEXT);
+		countdown();
+	} 
+
+	function countdown() {
+
+		if(count>0) {
+			$("#readyWindow").show();
+
+			setTimeout(function() {
+				
+				$("#readyText").html($("#readyText").html() + '.');
+
+				console.log(count);
+				count--;
+				countdown();
+			}, SECOND);
+
+		} else {
+			$("#readyWindow").hide();
+			$("#countWindow").show();
+
+			schedule_stressor();
+
+			setTimeout(function() {
+				$("#countWindow").hide();
+				inProgress = true;
+			}, TIMEOUT);
+		}
+
+	}
+
+	function fail() {
+		if(SHOWFAILIMAGES) {
+			$("#failImg").attr('src', IMAGEFOLDER + FAILIMAGES[Math.floor(Math.random()*FAILIMAGES.length)]);
+			$("#failImg").show();
+		} else {
+			$("#failButton").show();
+		}
+		$("#failWindow").show();
+		inProgress = false;
+		setTimeout(function() {
+			$("#failWindow").hide();
+			// $("#instructions").show();			
+			init();
+		}, TIMEOUT);
+	}
+
+	function schedule_stressor() {
+  		
+  		var stressor_schedule = Math.floor( Math.random() * ( 1 + STRESSINTERVAL_MAX - STRESSINTERVAL_MIN ) ) + STRESSINTERVAL_MIN;
+
+  		// show stressor window and hide after timeout
+  		setTimeout(function() {
+  			$("#stressWindow").show();
+  			setTimeout(function() {
+				hide_stressor();			
+	  		}, STRESS_WINDOW_DURATION);
+  		}, stressor_schedule);
+
+  		console.log('stressor scheduled in: ' + stressor_schedule + ' ms.');
+  	}
+
+  	function hide_stressor() {
+  		$("#stressWindow").hide();
+  	}
+
+});
+
